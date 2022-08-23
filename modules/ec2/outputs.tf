c46a26d85f1e9438e5721fbb343ce6fe8667962a
Shah Hiral <73848946+7984495444@users.noreply.github.com>
2022-08-23 11:37:23 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_id" {
-    value = aws_instance.web.id
+    value = aws_instance.myweb.id
 }
 
 output "ec2_pub_ip" {
-  value = aws_instance.web.public_ip
+  value = aws_instance.web.mypublic_ip
 }
